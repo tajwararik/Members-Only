3415b2c0534942e390bcb8da1c50f43eb2528cd7
tajwararik <tajwar.arik.10@gmail.com>
2025-11-23 19:32:29 +0600

    Added routes for signup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("node:path");
+const userValidation = require("./validators/userValidator");
+const userController = require("./controllers/userController");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,13 +11,21 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(3000, (error) => {
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", userValidation.userCreatePost, userController.createUser);
+
+app.listen(PORT, (error) => {
   if (error) throw error;
 
   console.log(`Connected on port: ${PORT}`);
