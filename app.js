1d34940a95840788724482245d1bcb0b33b02a6c
tajwararik <tajwar.arik.10@gmail.com>
2025-11-26 15:59:14 +0600

    Added session and implemented the routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("node:path");
 const userValidation = require("./validators/userValidator");
+const passport = require("./validators/passport");
 const userController = require("./controllers/userController");
 require("dotenv").config();
 
@@ -11,19 +13,42 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "keyboard-cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+function checkingAuthentication(req, res, next) {
+  if (req.isAuthenticated()) return next();
+
+  res.redirect("/login");
+}
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
+app.get("/signup", userController.getSignUpForm);
 app.post("/signup", userValidation.userCreatePost, userController.createUser);
+app.get("/login", userController.getLogInForm);
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  })
+);
+app.get("/home", checkingAuthentication, userController.getUserHome);
+app.get("/logout", userController.userLogOut);
+app.get("/join-club", userController.joinSecretClub);
 
 app.listen(PORT, (error) => {
   if (error) throw error;
