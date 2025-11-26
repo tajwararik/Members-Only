b567c1df3666988d1b1f4dc67a573bfed08f9722
tajwararik <tajwar.arik.10@gmail.com>
2025-11-26 15:58:07 +0600

    Created controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcryptjs");
 const userQueries = require("../db/queries");
 
+function getSignUpForm(req, res) {
+  res.render("signup");
+}
+
 async function createUser(req, res) {
   const { firstName, lastName, email, password } = req.validatedUser;
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -8,6 +12,31 @@ async function createUser(req, res) {
   res.redirect("/signup");
 }
 
+function getLogInForm(req, res) {
+  res.render("login");
+}
+
+function getUserHome(req, res) {
+  res.render("home", { user: req.user });
+}
+
+function userLogOut(req, res, next) {
+  req.logout((error) => {
+    if (error) return next(error);
+
+    res.redirect("/");
+  });
+}
+
+function joinSecretClub(req, res) {
+  res.render("join-club");
+}
+
 module.exports = {
+  getSignUpForm,
   createUser,
+  getLogInForm,
+  getUserHome,
+  userLogOut,
+  joinSecretClub,
 };
